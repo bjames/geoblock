4e0ff231effb5bf24914726991ec5120ff0494b2
Brandon James <brandon@brandonsjames.com>
2016-05-13 16:14:49 -0500

    Code cleanup and increased download speed
    Changed download to use urllib2 for a significant speed increase.
    Modified code to better match PEP8

--- geoblock.py
+++ geoblock.py
@@ -1,44 +1,52 @@
-import urllib, netaddr
+import urllib2
+import netaddr
 
-RIRS = ["afrinic", "apnic", "lacnic", "ripe", "arin"]
+RIRS = [["ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest", "afrinic"],
+        ["ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest", "apnic"],
+        ["ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest", "lacnic"],
+        ["ftp://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-latest", "ripe"],
+        ["ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest", "arin"]]
 
 
 def country_select():
 
-    user_input = raw_input("List of countries to [b]lock or to [p]ermit? [p]: ")
+    pref = raw_input("List of countries to [b]lock or to [p]ermit? [p]: ")
 
     try:
-        if user_input[0] == 'b':
+        if pref[0] == 'b':
             permit = False
-        elif user_input[0] == 'p':
+        elif pref[0] == 'p':
             permit = True
         else:
             print "Invalid input\n"
             return
 
-    # if we get an index error then nothing was entered, so our list is of countries to permit
+    # if we get an index error then nothing was entered
     except IndexError:
 
         permit = True
 
-    user_input = (raw_input("List countries using two character ISO3166 code, delimited with whitespace: ").upper()).split()
+    user_input = (raw_input("List countries using two character ISO3166 code, \
+                            delimited with whitespace: ").upper()).split()
 
     return user_input, permit
 
 
 def download_files():
 
-    # TODO File download is very slow. Find another method to download the files.
-
     print "Downloading data from RIRs\n"
 
-    urllib.urlretrieve("ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest", "afrinic")
-    urllib.urlretrieve("ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest", "apnic")
-    urllib.urlretrieve("ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest", "lacnic")
+    for rir in RIRS:
 
-    # RIPE and ARIN don't follow the standard format so we had to hardcode the URLs
-    urllib.urlretrieve("ftp://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-latest", "ripe")
-    urllib.urlretrieve("ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest", "arin")
+        # open the url and write the contents to our file (RIR[0] is our url
+        # RIR[1] is our filename
+        inet_file = urllib2.urlopen(rir[0])
+
+        with open(rir[1], 'w') as output:
+
+            output.write(inet_file.read())
+
+        output.close()
 
 
 def read_files(block_list, permit):
@@ -50,8 +58,6 @@ def read_files(block_list, permit):
     # where Key_1 is the Country Code, Key_2 is the IP Address as an integer
     # and the value is the number of IP address in the range
     # ex {'us', {823564: 1024}} Note: Those are just random numbers I typed
-    # Worth mentioning is that the number of addresses may not always be a power of 2
-    # more info on the file structure here: https://www.apnic.net/publications/media-library/documents/resource-guidelines/rir-statistics-exchange-format
 
     # list containing our file objects
     file_list = []
@@ -59,9 +65,9 @@ def read_files(block_list, permit):
     # nested dictionary used to store our countries and IP addresses
     country_ip = {}
 
-    # Open the files we downloaded earlier and store the file object in our list
+    # Open the files we downloaded earlier and store the file object
     for rir in RIRS:
-        file_list.append(open(rir))
+        file_list.append(open(rir[1]))
 
     for f in file_list:
 
@@ -74,37 +80,36 @@ def read_files(block_list, permit):
                 # we want only the ipv4 lines that are for a specific country
                 # also only want countries that we are going to block
                 if (curr_line[2] == "ipv4" and curr_line[1] != "*") and \
-                ((permit and curr_line[1] not in block_list) or (not permit and curr_line[1] in block_list)):
+                    ((permit and curr_line[1] not in block_list) or
+                     (not permit and curr_line[1] in block_list)):
 
                     country_code = curr_line[1]
                     ipv4_addr = netaddr.IPAddress(curr_line[3])
                     wildcard = netaddr.IPAddress(int(curr_line[4])-1)
 
-                    # ARIN has some addresses listed as 'reserved' that aren't assigned to any country
-                    # it doesn't seem like any of the other RIRs are doing this, but just in case we've handled
-                    # it here. I think the explanation is here: https://www.arin.net/policy/nrpm.html#four10
+                    # ARIN has some addresses listed as 'reserved' that aren't
+                    # assigned to any country it doesn't seem like any of the
+                    # other RIRs are doing this, but just in case we've handled
+                    # it here. explanation is here:
+                    # https://www.arin.net/policy/nrpm.html#four10
                     if country_code == '':
 
                         country_code = f.name + " reserved"
 
-                    # case if the country is already in the dict, we do not want to overwrite any keys
+                    # key is already in our dict
                     if country_ip.has_key(country_code):
 
-                        # curr_line[1] is the country code, curr_line[3] is the ip address (network id), curr_line[4]
-                        # is the count of address (not always CIDR)
                         country_ip[country_code][ipv4_addr] = wildcard
 
-                    # case if we do not yet have the country code in our nested dict
+                    # key is not in our dict
                     else:
 
-                        # this creates our first key-value pair for the nested dict of the current country, which tells
-                        # the Python interpreter that we are creating a dictionary
-                        # readability: country_ip[country_code]={1st_ipv4_address:num_addresses_in_range]
-                        country_ip[country_code]={ipv4_addr: wildcard}
+                        country_ip[country_code] = {ipv4_addr: wildcard}
 
             except IndexError:
 
-                # some of the lines aren't split into any columns. We don't need data from those anyways.
+                # some of the lines aren't split into any columns
+                # those don't have any data we need anyways
                 pass
 
     return country_ip
@@ -112,17 +117,16 @@ def read_files(block_list, permit):
 
 def gen_acl(country_ip):
 
-    print "Generating ACL\n"
-
-    # TODO add a mechanism to select which countries to block (or which countries to not block)
     # Generates Cisco IOS ACL with wildcard bits
+
+    print "Generating ACL\n"
 
     outfile = open('acl.txt', 'w')
 
     outfile.write('ip access-list extended geoblock\n')
     outfile.write('remark Generated using geoblock.py\nremark\n')
 
-    # Pretty standard, iterate over our dictionary and output the data to acl.txt
+    # iterate over our dictionary and output the data to acl.txt
     for country, ip_dict in country_ip.iteritems():
 
         outfile.write('remark Block IP ranges from ' + country + '\nremark\n')
@@ -130,9 +134,11 @@ def gen_acl(country_ip):
         for ip, wildcard in ip_dict.iteritems():
 
             # deny ip ip_address wildcard_bits
-            outfile.write('deny ip {0} {1} any\n'.format(str(ip), str(wildcard)))
+            outfile.write('deny ip {0} {1} any\n'.format(str(ip),
+                          str(wildcard)))
 
-        outfile.write('remark\nremark End of IP ranges from ' + country + '\nremark\n')
+        outfile.write('remark\nremark End of IP ranges from ' +
+                      country + '\nremark\n')
 
 
 def main():
